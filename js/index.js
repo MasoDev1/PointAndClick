6558ad77cde963092ccbde3193aec20e8d899e8a
MasoDev1 <R.masolijn@sintlucasedu.nl>
2023-03-22 16:37:52 +0100

    e3

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
 document.getElementById("main-Title").innerText = "Point and click adventure";
+
+const characterOffset = 16;
+
+const gameWindow = document.getElementById("gameWindow");
+const mainCharacter = document.getElementById("mainCharacter");
+
+gameWindow.onclick = function(e){
+    var rect = gameWindow.getBoundingClientRect();
+    var x = e.clientX - rect.left;
+    var y = e.clientY - rect.top;
+
+    mainCharacter.style.left = x- characterOffset + "px";
+    mainCharacter.style.top = y - characterOffset + "px";
+
+    switch(e.target.id){
+        case "door1":
+        console.log("grrr get away baby dont hurt me");
+        break;
+        case "default":
+            console.log("wrist so icy wonder why she like me bitch im drowning")
+            break;
+        
+
+    }
+
+    console.log(e.target.id);
+}
